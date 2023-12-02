0db98e70bdebd1539e96e07ad3b3d2b37cb75a7e
Ali Haider <147542364+gemdev143@users.noreply.github.com>
2023-12-03 02:52:54 +0500

    Update HomeBaner.jsx

--- src/components/Baner/HomeBaner.jsx
+++ src/components/Baner/HomeBaner.jsx
@@ -1,31 +1,38 @@
 import styles from "../../../styles/HomeBaner.module.css";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
 
-export default function HomeBaner({ dataBanner }) {
+// Install Swiper components
+SwiperCore.use([Pagination, Navigation]);
+
+export default function HomeBanner({ dataBanner }) {
   return (
-    <div className={styles.home_baner_container}>
+    <div className={styles.home_banner_container}>
       <div className={styles.section}>
-        <h2 className="yellow">Our Company</h2>
+        <h2 className="yellow">Our Brand Partners</h2>
       </div>
-      <Link
-        href={dataBanner?.url}
-        style={{
-          background: `url(${dataBanner.image})`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
         }}
-        target="_blank"
-        className={styles.section1}
+        modules={[Pagination]}
+        className="mySwiper3"
       >
-        <div>
-          <h3>{dataBanner?.title}</h3>
-          <Link className="color_yellow" target="_blank" href={dataBanner?.url}>
-            More Details
-          </Link>
-        </div>
-      </Link>
-      {/* </Link> */}
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 }
