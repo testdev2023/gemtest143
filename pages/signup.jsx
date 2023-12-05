eb2376b3e901e2d8d0e61fe1cc8c19b480cae834
Ali Haider <147542364+gemdev143@users.noreply.github.com>
2023-12-05 21:53:06 +0500

    Update signup.jsx

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -2,6 +2,9 @@ import styles from "../styles/Signup.module.css";
 // import Slider from "@/src/components/slider/Slider";
 import Slider from "../src/components/slider/Slider";
 import SignupStep1 from "../src/components/SignupSteps/SignupStep1";
+import TopBar from "../src/components/TopBar/EntTopBar";
+import Footer from "../src/components/Footer/Footer";
+import ScrollToTopButton from "../src/components/ScrollToTopButton/ScrollToTopButton";
 
 export default function Signup() {
   return (
@@ -9,7 +12,10 @@ export default function Signup() {
       <div className={styles.parent}>
         {/* <Slider /> */}
         <div className={styles.signup_sub}>
+        <ScrollToTopButton />
+        <TopBar />
           <SignupStep1 />
+          <Footer />
         </div>
       </div>
     </div>
