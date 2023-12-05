2d5a558105cc23010bcdddbff4a11c67f4ce7440
Ali Haider <147542364+gemdev143@users.noreply.github.com>
2023-12-05 21:05:17 +0500

    Update HomeBaner.jsx

--- src/components/Baner/HomeBaner.jsx
+++ src/components/Baner/HomeBaner.jsx
@@ -1,7 +1,7 @@
 import styles from "../../../styles/HomeBaner.module.css";
 import Link from "next/link";
-import Banner from "../../asset/Logo-1.jpg"
-import Banner2 from "../../asset/Logo-2.jpg"
+import Banner from "../../asset/Logo-1.jpg";
+import Banner2 from "../../asset/Logo-2.jpg";
 
 export default function HomeBaner({ dataBanner }) {
   return (
@@ -9,27 +9,10 @@ export default function HomeBaner({ dataBanner }) {
       <div className={styles.section}>
         <h2 className="yellow">Our Brand Partners</h2>
       </div>
-      <img src="../../asset/Logo-1.jpg"/>
-      <img src="../../asset/Logo-2.jpg"/>
-      {/* <Link
-        href={dataBanner?.url}
-        style={{
-          background: `url(${dataBanner.image})`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-        target="_blank"
-        className={styles.section1}
-      >
-        <div>
-          <h3>{dataBanner?.title}</h3>
-          <Link className="color_yellow" target="_blank" href={dataBanner?.url}>
-            More Details
-          </Link>
-        </div>
-      </Link> */}
-      {/* </Link> */}
+      <img src={Banner} alt="Banner 1" />
+      {/* If you have a second banner, you can use it like this */}
+      <img src={Banner2} alt="Banner 2" />
+      {/* Rest of your code */}
     </div>
   );
 }
