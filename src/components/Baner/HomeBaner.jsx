63f192cbb1a50ad73017aa2101c35dc3a88f07c6
Ali Haider <147542364+gemdev143@users.noreply.github.com>
2023-12-10 02:37:02 +0500

    Update HomeBaner.jsx

--- src/components/Baner/HomeBaner.jsx
+++ src/components/Baner/HomeBaner.jsx
@@ -3,12 +3,11 @@ import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/swiper.min.css';
 import 'swiper/components/autoplay/autoplay.min.css';
 import styles from "../../../styles/HomeBaner.module.css";
-import Link from "next/link";
 
 // Install Swiper modules
 SwiperCore.use([Autoplay]);
 
-export default function HomeBaner({ dataBanner }) {
+export default function HomeBaner() {
   return (
     <div className={styles.home_baner_container}>
       <div className={styles.section}>
@@ -22,43 +21,37 @@ export default function HomeBaner({ dataBanner }) {
         autoplay={{ delay: 3000 }}
         className={styles.swiperContainer}
       >
-        {/* Map through your dataBanner array */}
-        {dataBanner.map((banner, index) => (
-          <SwiperSlide key={index}>
-            <Link
-              href={banner.url}
-              style={{
-                background: `url(${banner.image})`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-              }}
-              target="_blank"
-              className={styles.section1}
-            >
-              <div>
-                <h3>{banner.title}</h3>
-                <Link className="color_yellow" target="_blank" href={banner.url}>
-                  More Details
-                </Link>
-              </div>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+        {/* Static Slides */}
+        <SwiperSlide>
+          <div className={styles.staticImageSection}>
+            <img
+              src="/path/to/your/static/image1.jpg"
+              alt="Static Image 1"
+              className={styles.staticImage}
+            />
+            <div>
+              <h3>Static Image Title 1</h3>
+              <p>Static Image Description 1</p>
+            </div>
+          </div>
+        </SwiperSlide>
 
-      {/* Static Image Section */}
-      <div className={styles.staticImageSection}>
-        <img
-          src="/path/to/your/static/image.jpg"
-          alt="Static Image"
-          className={styles.staticImage}
-        />
-        <div>
-          <h3>Static Image Title</h3>
-          <p>Static Image Description</p>
-        </div>
-      </div>
+        <SwiperSlide>
+          <div className={styles.staticImageSection}>
+            <img
+              src="/path/to/your/static/image2.jpg"
+              alt="Static Image 2"
+              className={styles.staticImage}
+            />
+            <div>
+              <h3>Static Image Title 2</h3>
+              <p>Static Image Description 2</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        {/* Add more static slides as needed */}
+      </Swiper>
     </div>
   );
 }
