9cd3cfd6295f6eff95eda75aa74d7a0ac26cb8c9
Ali Haider <147542364+gemdev143@users.noreply.github.com>
2023-12-10 02:33:55 +0500

    Update HomeBaner.jsx

--- src/components/Baner/HomeBaner.jsx
+++ src/components/Baner/HomeBaner.jsx
@@ -1,5 +1,12 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/components/autoplay/autoplay.min.css';
 import styles from "../../../styles/HomeBaner.module.css";
 import Link from "next/link";
+
+// Install Swiper modules
+SwiperCore.use([Autoplay]);
 
 export default function HomeBaner({ dataBanner }) {
   return (
@@ -7,25 +14,38 @@ export default function HomeBaner({ dataBanner }) {
       <div className={styles.section}>
         <h2 className="yellow">Our Brand Partners</h2>
       </div>
-      <Link
-        href={dataBanner?.url}
-        style={{
-          background: `url(${dataBanner.image})`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-        target="_blank"
-        className={styles.section1}
+
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={3} // Adjust the number of slides per view as needed
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        className={styles.swiperContainer}
       >
-        <div>
-          <h3>{dataBanner?.title}</h3>
-          <Link className="color_yellow" target="_blank" href={dataBanner?.url}>
-            More Details
-          </Link>
-        </div>
-      </Link>
-      {/* </Link> */}
+        {/* Map through your dataBanner array */}
+        {dataBanner.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <Link
+              href={banner.url}
+              style={{
+                background: `url(${banner.image})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+              target="_blank"
+              className={styles.section1}
+            >
+              <div>
+                <h3>{banner.title}</h3>
+                <Link className="color_yellow" target="_blank" href={banner.url}>
+                  More Details
+                </Link>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
